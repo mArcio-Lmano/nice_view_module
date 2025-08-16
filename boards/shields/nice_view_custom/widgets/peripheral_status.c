704c80ac227b977cbf7ec6ebf8fe01d325625574
mArcio-Lmano <marcio.mano2322@gmail.com>
2025-08-16 16:51:19 +0100

    New: love image

--- boards/shields/nice_view_custom/widgets/peripheral_status.c
+++ boards/shields/nice_view_custom/widgets/peripheral_status.c
@@ -126,9 +126,9 @@ int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent) {
                        LV_IMG_CF_TRUE_COLOR);
 
   lv_obj_t *art = lv_img_create(widget->obj);
-  // bool random = sys_rand32_get() & 1;
-  // lv_img_set_src(art, random ? &balloon : &mountain);
-  lv_img_set_src(art, &image);
+  bool random = sys_rand32_get() & 1;
+  lv_img_set_src(art, random ? &image : &love);
+  // lv_img_set_src(art, &image);
   lv_obj_align(art, LV_ALIGN_TOP_LEFT, 0, 0);
 
   sys_slist_append(&widgets, &widget->node);
