c14e2a3535e83558038f943c38e0ae261416ac06
mArcio-Lmano <marcio.mano2322@gmail.com>
2025-08-16 17:01:17 +0100

    Fix: the love image not defined

--- boards/shields/nice_view_custom/widgets/art.c
+++ boards/shields/nice_view_custom/widgets/art.c
@@ -140,24 +140,6 @@ const lv_img_dsc_t image = {
     .data = image_map,
 };
 
-#ifdef __has_include
-#if __has_include("lvgl.h")
-#ifndef LV_LVGL_H_INCLUDE_SIMPLE
-#define LV_LVGL_H_INCLUDE_SIMPLE
-#endif
-#endif
-#endif
-
-#if defined(LV_LVGL_H_INCLUDE_SIMPLE)
-#include "lvgl.h"
-#else
-#include "lvgl/lvgl.h"
-#endif
-
-#ifndef LV_ATTRIBUTE_MEM_ALIGN
-#define LV_ATTRIBUTE_MEM_ALIGN
-#endif
-
 #ifndef LV_ATTRIBUTE_IMG_LOVE
 #define LV_ATTRIBUTE_IMG_LOVE
 #endif
